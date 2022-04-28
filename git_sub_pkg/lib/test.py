a7f8f96c7d7efc4bc4ee7fa3e14251774354e2d9
Kenta Fukuhara <KAkikaku.tateyama@gmail.com>
2022-04-28 13:17:38 +0900

    [Add] docstring

--- git_sub_pkg/lib/test.py
+++ git_sub_pkg/lib/test.py
@@ -1,6 +1,14 @@
 import numpy as np
 
 def get_square(num:int) -> np.ndarray:
+    """ create zero array
+
+    Args:
+        num (int): shape
+
+    Returns:
+        np.ndarray: zero squeare array
+    """
     return np.zeros((num, num))
 
 def get_square_ones(num:int) -> np.ndarray:
